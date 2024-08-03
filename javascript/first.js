41fd685b42b0522bb03fa6a6b6258f8dedd6c40a
Abhinavthakur10 <161066057+Abhinavthakur10@users.noreply.github.com>
2024-08-03 23:37:44 +0530

    js basic

---
+++ javascript/first.js
@@ -0,0 +1,25 @@
+// console.log(1);
+// console.log("Abhinav");
+
+//                 variable in java script                 //
+//  variable can change  
+var name="scott";
+var age=20;
+// var name="scott",age=20;
+console.log(name);
+console.log(age);
+
+const hourperday=24;//contant which cannot change 
+
+
+/*             operator in javascript         */
+
+let a =10, b=3;
+let d=a**b  // airthmetical operator
+let c=a+b;
+
+var l=100;
+ l +=10;//a=a+10
+ 
+ console.log(l);
+
