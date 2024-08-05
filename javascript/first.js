7916797b7b142d2b2ba47c7cf46d600ea65af8fc
Abhinavthakur10 <161066057+Abhinavthakur10@users.noreply.github.com>
2024-08-05 19:44:59 +0530

    baisc js

--- javascript/first.js
+++ javascript/first.js
@@ -20,6 +20,32 @@ let c=a+b;
 
 var l=100;
  l +=10;//a=a+10
- 
+
  console.log(l);
+
+ var m=10;
+ console.log(m);
+ var j=++a;// m=11 first, b=11 (pre increment)
+ console.log(a,b);
+
+ var x=10;
+ console.log(x);
+ var z=x++;// x=10 return irst, a=11(post increment)
+ console.log(x,z)
+
+ var r=10; y=11;
+ console.log(r==y);
+ console.log(r!=y);  //realtional operator
+
+ 
+//logical operator((&&-one statment is true)
+                //  (||- one statment is false))
+ var q=10,n=20, t=10;
+ var k= q==t && q>c;// true or false 
+ 
+ console.log(k);
+
+
+
+
 
